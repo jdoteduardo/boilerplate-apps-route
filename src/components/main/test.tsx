beb1fc80f62dc0bcc018f6d087d925b179f224ec
jdoteduardo <joseeduardo0131@gmail.com>
2025-04-30 22:36:52 -0300

    Keep calm and commit 4

--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -5,7 +5,9 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /aaaaaa/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
